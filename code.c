5f23c945c40393d51a3a14d0e5b5ca40e3fb0738
amineziadi <amine.ziadi1002@gmail.com>
2022-10-18 17:04:59 -0400

    back to sixth commit

--- code.c
+++ code.c
@@ -2,9 +2,13 @@
 int main() {
    // printf() displays the string inside quotation
 <<<<<<< HEAD
+<<<<<<< HEAD
    printf("8 commit");
 =======
    printf("Second commit");
 >>>>>>> parent of e08d7b7 (third commit)
+=======
+   printf("fifth commit");
+>>>>>>> parent of 92b7e85 (sixth commit)
    return 0;
 }

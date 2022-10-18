460f325c3393cd49dfc015afdd1b1cd8fa7168a8
amineziadi <amine.ziadi1002@gmail.com>
2022-10-18 17:02:56 -0400

    back to third commit

--- code.c
+++ code.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
+<<<<<<< HEAD
    printf("8 commit");
+=======
+   printf("Second commit");
+>>>>>>> parent of e08d7b7 (third commit)
    return 0;
 }
